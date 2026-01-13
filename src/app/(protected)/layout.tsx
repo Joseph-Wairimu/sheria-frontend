04384fdce2aba28a61ed313ff4c2e53fc5d5993b
Joseph-Wairimu <josephkamore084@gmail.com>
2026-01-13 20:26:36 +0300

    update: get token

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,22 +1,18 @@
 import MainLayout from "@/src/components/layout/MainLayout";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default async function ProtectedLayout({
+export default function ProtectedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  function getCookie(name: string): string | undefined {
-    if (typeof window === "undefined") return undefined;
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop()?.split(";").shift();
-    return undefined;
-  }
-  const accessToken = getCookie("access_token");
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get("access_token");
   console.log("ProtectedLayout accessToken:", accessToken);
   if (!accessToken) {
     redirect("/login");
   }
+
   return <MainLayout>{children}</MainLayout>;
 }
