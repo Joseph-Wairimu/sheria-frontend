e6ed71097d71ec03c9efdc360fb4e4d0b911174a
Joseph-Wairimu <josephkamore084@gmail.com>
2026-01-13 20:16:22 +0300

    update: cookie check

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,10 +1,21 @@
-import MainLayout from '@/src/components/layout/MainLayout';
+import MainLayout from "@/src/components/layout/MainLayout";
+import { redirect } from "next/navigation";
 
 export default async function ProtectedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
+  function getCookie(name: string): string | undefined {
+    if (typeof window === "undefined") return undefined;
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop()?.split(";").shift();
+    return undefined;
+  }
+  const accessToken = getCookie("access_token");
+  if (!accessToken) {
+    redirect("/login");
+  }
   return <MainLayout>{children}</MainLayout>;
 }
