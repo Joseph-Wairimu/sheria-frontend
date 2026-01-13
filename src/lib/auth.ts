d386e68737175b738bd80857d433e66a1637bae3
Joseph-Wairimu <josephkamore084@gmail.com>
2026-01-13 17:03:41 +0300

    update: add cf clearance

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,42 +1,46 @@
-import { env } from 'next-runtime-env'
 import { cookies } from 'next/headers'
+import { env } from 'next-runtime-env'
 
 export async function exchangeToken() {
-    console.log('Exchange Token Hit:');
-    const AUTH_BASE_URL = env('AUTH_BASE_URL')
-    const res = await fetch(
-        `${AUTH_BASE_URL}/auth/tokens/exchange`,
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            credentials: 'include',
-            cache: 'no-store',
-        }
-    )
-    console.log('Exchange Token Response:');
-    console.log('Exchange Token Response:', res);
-    if (!res.ok) {
-        return null
-    }
+  console.log('Exchange Token Hit')
 
-    const data = await res.json()
+  const cookieStore = cookies()
+  const cfClearance = cookieStore.get('cf_clearance')?.value
 
-    console.log('Exchange Token Response:', data);
+  if (!cfClearance) {
+    console.error('No cf_clearance cookie found')
+    return null
+  }
 
-    const accessToken = data?.details?.token
+  const AUTH_BASE_URL = env('AUTH_BASE_URL')
 
-    if (!accessToken) return null
+  const res = await fetch(`${AUTH_BASE_URL}/auth/tokens/exchange`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Cookie': `cf_clearance=${cfClearance}`,
+    },
+    cache: 'no-store',
+  })
 
-    cookies().set({
-        name: 'access_token',
-        value: accessToken,
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax',
-        path: '/',
-    })
+  console.log('Exchange Token Response status:', res.status)
 
-    return accessToken
+  if (!res.ok) return null
+
+  const data = await res.json()
+ console.log('Exchange Token Response:', data);
+  const accessToken = data?.details?.token
+
+  if (!accessToken) return null
+
+  cookies().set({
+    name: 'access_token',
+    value: accessToken,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'lax',
+    path: '/',
+  })
+
+  return accessToken
 }
