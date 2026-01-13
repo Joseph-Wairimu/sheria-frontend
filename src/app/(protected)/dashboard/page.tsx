6c95df4fe417ac9df26289c6362ab0f9c6f89e56
Joseph-Wairimu <josephkamore084@gmail.com>
2026-01-13 20:08:31 +0300

    update: remove dashboard token check

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -26,8 +26,7 @@ import {
 } from '@mui/icons-material';
 import { useRouter } from 'next/navigation';
 import StatCard from '@/src/components/common/StatCard';
-import { redirect } from "next/navigation";
-import Cookies from "js-cookie";
+
 
 export default function HomePage() {
   const router = useRouter();
@@ -73,11 +72,7 @@ export default function HomePage() {
     { title: 'Active Users', value: '12,459', icon: People, color: '#f59e0b' },
     { title: 'Avg Response Time', value: '0.8s', icon: Speed, color: '#ec4899' },
   ];
-  const accessToken = Cookies.get("access_token");
 
-  if (!accessToken) {
-    redirect("/login");
-  }
   return (
     <Box>
       {/* Hero Section */}
