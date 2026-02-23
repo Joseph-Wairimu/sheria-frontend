b9e8aee5b88fc46666fec549bfc26e1ca133be46
Joseph-Wairimu <josephkamore084@gmail.com>
2026-02-23 21:25:27 +0300

    fix: cookies

--- src/app/actions/auth.tsx
+++ src/app/actions/auth.tsx
@@ -39,7 +39,7 @@ export async function loginAction(
     cookies().set('access_token', access_token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'lax',
       path: '/',
       maxAge: 60 * 60 * 24 * 7, 
     });
